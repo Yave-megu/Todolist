c636a0c526bee451ad5d9289cf62799f2179a64d
gimdaehan <dongsime@gmail.com>
2022-09-29 13:59:37 +0900

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by 김대한 on 2022/09/29.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
